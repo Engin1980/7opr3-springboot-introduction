42553dbac66d8f7157d819e1fc85373f7855c55b
Marek Vajgl <marek.vajgl@osu.cz>
2023-09-28 11:18:59 +0200

    Přidáno zpracování chyb u EventController

---
+++ src/main/java/cz/osu/kip/eventReminder/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package cz.osu.kip.eventReminder.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+  @ExceptionHandler(NoSuchElementException.class)
+  public ResponseEntity<String> noSuchElementExceptionHandler(NoSuchElementException ex, WebRequest request){
+    return new ResponseEntity<>("Element not found", HttpStatus.NOT_FOUND);
+  }
+
+}
--- src/main/java/cz/osu/kip/eventReminder/controllers/EventController.java
+++ src/main/java/cz/osu/kip/eventReminder/controllers/EventController.java
@@ -5,10 +5,15 @@ import cz.osu.kip.eventReminder.services.EventService;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/event")
@@ -28,6 +33,48 @@ public class EventController {
     Event ret = this.eventService.getById(eventId).orElse(null);
     return ret;
   }
+
+// ***
+// Zpracování chyby při nenalezení přes ResponseEntity
+// ***
+//  @GetMapping
+//  public ResponseEntity<?> getById(@RequestParam int eventId) {
+//    Optional<Event> event = this.eventService.getById(eventId);
+//    ResponseEntity<?> ret;
+//
+//    if (event.isPresent())
+//      ret = ResponseEntity.ok(event);
+//    else
+//      ret = new ResponseEntity<>("Event with eventId=" + eventId + " not found.", HttpStatus.NOT_FOUND);
+//
+//    return ret;
+//  }
+
+
+// ***
+// Zpracování chyby při nenalezení přes ResponseStatusException
+// ***
+//  @GetMapping
+//  public Event getById(@RequestParam int eventId) {
+//    Optional<Event> event = this.eventService.getById(eventId);
+//
+//    if (event.isEmpty())
+//      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Event with eventId=" + eventId + " not found.");
+//
+//    return event.get();
+//  }
+
+
+// ***
+// Zpracování chyby při nenalezení přes ControllerExceptionHandler
+// ***
+//  @GetMapping
+//  public Event getById(@RequestParam int eventId) {
+//    return this.eventService
+//            .getById(eventId)
+//            .orElseThrow(() -> new NoSuchElementException(
+//                    "Event with eventId=" + eventId + " not found."));
+//  }
 
   @GetMapping("/list")
   public List<Event> list() {
